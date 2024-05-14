41cb6b22377b7f818ed422a4f4fe5a885bcc6949
John Doe <johndoe@email.com>
2024-05-14 14:36:38 +0200

    Assertions added

--- tests/swag.labs.spec.ts
+++ tests/swag.labs.spec.ts
@@ -1,4 +1,5 @@
 import {test, expect} from '@playwright/test';
+import { escape } from 'querystring';
 
 test.beforeEach ("Navigating to the page", async ({page}) => {
 
@@ -10,6 +11,7 @@ await expect.soft(page).toHaveURL("https://www.saucedemo.com/");
 await page.getByPlaceholder("Username").fill('standard_user');
 await page.getByPlaceholder('Password').fill('secret_sauce');
 await page.locator('#login-button').click();
+await expect.soft(page).toHaveTitle('Swag Labs');
 
 });
 
@@ -17,6 +19,7 @@ test ("Buy product", async ({page}) => {
 
 await page.locator('#add-to-cart-sauce-labs-backpack').click();
 await page.locator('.shopping_cart_link').click();
+await expect(page.locator('.inventory_item_name')).toHaveText('Sauce Labs Backpack');
 await page.locator('#checkout').click();
 await page.getByPlaceholder('First Name').fill("Peter");
 await page.getByPlaceholder('Last Name').fill("Pan");
@@ -24,6 +27,7 @@ await page.getByPlaceholder('Zip/Postal Code').fill("2587D");
 await page.locator('#continue').click();
 await page.locator('#finish').click();
 await page.locator('Back Home');
+await expect(page.locator('.complete-header')).toHaveText('Thank you for your order!');
 
 });
 
@@ -32,6 +36,7 @@ test ("Remove product from cart", async ({page}) => {
 await page.locator('#add-to-cart-sauce-labs-backpack').click();
 await page.locator('.shopping_cart_link').click();
 await page.locator('#remove-sauce-labs-backpack').click();
+await expect(page.locator('.removed_cart_item')).toBeTruthy();
 
 });
 
@@ -43,11 +48,13 @@ await page.locator('.shopping_cart_link').click();
 await page.locator('#continue-shopping').click();
 await page.locator('#remove-sauce-labs-backpack').click();
 await page.locator('#remove-sauce-labs-bike-light').click();
+await expect(page.locator('#add-to-cart-sauce-labs-backpack')).toHaveText('Add to cart');
+await expect(page.locator('#add-to-cart-sauce-labs-bike-light')).toHaveText('Add to cart');
 
 });
 
 test ("Change product sorting", async ({page}) => {
 
 await page.locator('data-test=product-sort-container').selectOption('Name (Z to A)');
-
+await expect(page.locator('data-test=active-option')).toHaveText('Name (Z to A)');
 });
